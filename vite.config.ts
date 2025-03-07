19e79ab0437ab5d4f5bdf34f3759e7901eca2d68
Daniil <daniil.axiyan.316@mail.ru>
2025-03-07 13:27:36 +0300

    refactor config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: '/newsTest',
+	base: '/news-test',
 	plugins: [react()],
 	resolve: {
 		alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
